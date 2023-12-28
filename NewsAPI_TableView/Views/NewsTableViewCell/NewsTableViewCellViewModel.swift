8b931e399e9407b1404c93ecfb1966d674bffb25
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2023-12-28 17:32:59 +0400

    feat: add pull-to-refresh

--- NewsAPI_TableView/Views/NewsTableViewCell/NewsTableViewCellViewModel.swift
+++ NewsAPI_TableView/Views/NewsTableViewCell/NewsTableViewCellViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class NewsTableViewCellViewModel {
+    let title: String
+    let description: String
+    let newsUrl: URL
+    let imageUrl: URL
+    var imageData: Data? = nil
+    
+    init(
+        title: String,
+        description: String,
+        imageUrl: URL,
+        newsUrl: URL
+    ) {
+        self.title = title
+        self.description = description
+        self.newsUrl = newsUrl
+        self.imageUrl = imageUrl
+    }
+}
